b3105a651b5decb3d62f94bfde9248caf740ca03
tien06051992 <tien06051992@gmail.com>
2013-09-11 14:21:01 +0700

    1) i have fixed layout  #container .container

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 jQuery(document).ready(function($) {
 
+            
+ // hides the slickbox as soon as the DOM is ready
+  $('#uRandom').hide();
+ // shows the slickbox on clicking the noted link  
+  $('#lirandom').click(function() {
+    $('#uRandom').show('slow');
+    return false;
+  });
+ // hides the slickbox on clicking the noted link  
+  $('#linews').click(function() {
+    $('#uRandom').hide('fast');
+    return false;
+  });
+ 
+ // toggles the slickbox on clicking the noted link  
+  
+
 });
