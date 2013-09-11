6f3c65a0565951aa935fe2b911676cbf63328b6b
tien06051992 <tien06051992@gmail.com>
2013-09-11 12:01:26 +0700

    1)e bo file nao ko fai cua bootstrap vao libs roi do a. 2)bo cai dong add jquery du lun

---
+++ js/libs/AnHien.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+        $(document).ready(function() {
+            
+ // hides the slickbox as soon as the DOM is ready
+  $('#uRandom').hide();
+ // shows the slickbox on clicking the noted link  
+  $('#lirandom').click(function() {
+    $('#uRandom').show('slow');
+    return false;
+  });
+ // hides the slickbox on clicking the noted link  
+  $('#linews').click(function() {
+    $('#uRandom').hide('fast');
+    return false;
+  });
+ 
+ // toggles the slickbox on clicking the noted link  
+  
+});
+
+
