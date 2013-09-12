55edb5613dd7a519a9da7006f217ec661fc3c6b6
tien06051992 <tien06051992@gmail.com>
2013-09-12 11:10:35 +0700

    1)nav_tab works properly with Jquery

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 jQuery(document).ready(function($) {      
- // hides the slickbox as soon as the DOM is ready
-  $('#uRandom').hide();
- // shows the slickbox on clicking the noted link  
-  $('#lirandom').click(function() {
-    $('#uRandom').show('slow');
-    return false;
-  });
- // hides the slickbox on clicking the noted link  
-  $('#linews').click(function() {
-    $('#uRandom').hide('fast');
-    return false;
-  });
- 
- // toggles the slickbox on clicking the noted link  
-  
-
+ //an tat ca tab_content
+ $('.tab_content:not(:first)').hide();
+ $('.tab li a').click(function(){
+    $('.tab li a').removeClass('active');
+    $(this).addClass('active');
+    $('.tab_content').hide('slow');
+    
+    var activeTab = $(this).attr('href');
+    //activeTab = #news //active = #random
+    $(activeTab).show('slow');
+        return false;
+ });
 });
