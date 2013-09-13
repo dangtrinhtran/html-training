1c3d26bd85b8e6bf9fed2ae7159ae0755bf528e3
tien06051992 <tien06051992@gmail.com>
2013-09-14 00:05:12 +0700

    1) do like design, font - arial - padding - margin 2)tab still can't be used Bootstrap Jquery

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
-jQuery(document).ready(function($) {      
- //an tat ca tab_content
- $('.tab_content:not(:first)').hide();
- $('.tab li a').click(function(){
-    $('.tab li a').removeClass('active');
-    $(this).addClass('active');
-    $('.tab_content').hide('slow');
+jQuery(document).ready(function($) {  
+    //********CODE THEO VIDEO*******//
+ //an tat ca tab_content trừ cái đầu tiên
+// $('.tab_content:not(:first)').hide();
+// $('.tab li a').click(function(){
+//    $('.tab li a').removeClass('active');
+//    $(this).addClass('active');
+//    $('.tab_content').hide('slow');
+//    
+//    var activeTab = $(this).attr('href');
+//    //activeTab = #news //active = #random
+//    $(activeTab).show('slow');
+//        return false;
+ //});
+    //********END CODE THEO VIDEO*******//
     
-    var activeTab = $(this).attr('href');
-    //activeTab = #news //active = #random
-    $(activeTab).show('slow');
-        return false;
- });
+    //********BOOTSTRAP JQUERY******//
+//    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+//    e.target // activated tab
+//    e.relatedTarget // previous tab
+//    });
+    
+    $('#myTab a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+    });
+    
 });
