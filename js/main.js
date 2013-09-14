98f8371299a03f6da5c11190860cbdd9181032ba
tien06051992 <tien06051992@gmail.com>
2013-09-14 09:42:51 +0700

    1) fixed hover li

--- js/main.js
+++ js/main.js
@@ -1,28 +1,7 @@
 jQuery(document).ready(function($) {  
-    //********CODE THEO VIDEO*******//
- //an tat ca tab_content trừ cái đầu tiên
-// $('.tab_content:not(:first)').hide();
-// $('.tab li a').click(function(){
-//    $('.tab li a').removeClass('active');
-//    $(this).addClass('active');
-//    $('.tab_content').hide('slow');
-//    
-//    var activeTab = $(this).attr('href');
-//    //activeTab = #news //active = #random
-//    $(activeTab).show('slow');
-//        return false;
- //});
-    //********END CODE THEO VIDEO*******//
-    
-    //********BOOTSTRAP JQUERY******//
-//    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-//    e.target // activated tab
-//    e.relatedTarget // previous tab
-//    });
-    
+    $('#myTab a[href="#random"]').tab('show');
     $('#myTab a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
+    e.preventDefault();
+     $(this).tab('show');
     });
-    
 });
